78c41e9b2d43d4da5c6c0b690a5221d83feb6f96
prachalgoyal03 <35257323+prachalgoyal03@users.noreply.github.com>
2018-10-15 14:28:18 +0530

    web template added

---
+++ Git web template/ongoing.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ongoing Projects</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+        <link rel="stylesheet" href="css1/main.css">
+
+  <style type="text/css">
+    body{
+      font-family: 'Rubik', sans-serif;
+    }
+  </style>
+</head>
+<body>
+    <div class="container main-menu">
+            <div class="row align-items-center justify-content-between d-flex">
+              <div id="logo">
+               <!--  <a href="index.html"><img src="logo2.png" alt="" title="" /></a> -->
+               <h2>Title here</h2>
+              </div>
+              <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                  <li class="menu-active"><a href="index.html">Home</a></li>
+                  <li><a href="about.html">comptitive programming</a></li>
+                  <li><a href="services.html">internships</a></li>
+                  
+                <li><a href="get your location.html">Profile</a></li>
+                                      
+                  <li><a href="contact.html">Ongoing Projects</a></li>
+                   <li><a href="about.html">Login</a></li>
+                  <li><a href="services.html">register</a></li>
+                   
+              </ul>
+              </nav><!-- #nav-menu-container -->            
+            </div>
+          </div>
+        </header><!-- #header -->
+        <br>
+        <br>
+<div class="container">
+ <center> <h1>Currently Ongoing Projects in Global Institute Of Technology</h1> </center>
+  <br>
+  <?php
+$con=mysqli_connect("localhost","root","","username");
+$query="Select * from ongoing";
+$result = mysqli_query($con,$query);
+echo "<table class='table table-striped' >
+  <thead>
+<tr>
+<th>Project name</th>
+<th>Domain</th>
+<th>Description</th>
+<th>Collabs</th>
+</tr>
+</thead>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['domain'] . "</td>";
+echo "<td>" . $row['name'] . "</td>";
+echo "<td>" . $row['desshort'] . "</td>";
+echo "<td>
+<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#myModal'>
+    Interested
+  </button>
+
+  <!-- The Modal -->
+  <div class='modal' id='myModal'>
+    <div class='modal-dialog'>
+      <div class='modal-content'>
+      
+        <!-- Modal Header -->
+        <div class='modal-header'>
+          <h4 class='modal-title'>Project Description</h4>
+          <button type='button' class='close' data-dismiss='modal'>&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class='modal-body'>"
+         .$row['deslarge'].
+
+       "</div>
+        
+        
+        <!-- Modal footer -->
+        <div class='modal-footer'>
+          <div>
+          <center> <button type='button' class='btn btn-primary' >
+          Apply Now
+          </button></center>
+        </div>
+          <button type='button' class='btn btn-danger' data-dismiss='modal'>Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+   </td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
+<!--   <p>The .table-striped class adds zebra-stripes to a table:</p>            
+ -->  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Project Name</th>
+        <th>Domain</th>
+        <th>Description</th>
+        <th>Collab</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Smart Energy optimization system</td>
+        <td>Interenet of Things</td>
+        <td>john@example.com cjcjkcjhk</td>
+        <td><!-- <button type="button" class="btn btn-primary">Interested</button> -->
+         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+    Interested
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Project Description</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          The project is based on INternet of things and Machine Learning, we used bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+                    The project is based on INternet of things and Machine Learning, we used bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+
+        </div>
+        
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <div>
+          <center> <button type="button" class="btn btn-primary" >
+          Apply Now
+          </button></center>
+        </div>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+        </td>
+      </tr>
+      <tr>
+        <td>fuck fuck fuck</td>
+        <td>ML</td>
+        <td>mary@example.com</td>
+        <td><button type="button" class="btn btn-primary">Interested</button></td>
+      </tr>
+      <tr>
+        <td>shit shit shit shits hsot</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+        <td><button type="button" class="btn btn-primary">Interested</button></td>
+      </tr>
+      <tr>
+        <td>shit shit shit shits hsot</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+        <td><button type="button" class="btn btn-primary">Interested</button></td>
+      </tr>
+      <tr>
+        <td>Smart Energy optimization system</td>
+        <td>Interenet of Things</td>
+        <td>john@example.com</td>
+        <td><button type="button" class="btn btn-primary">Interested</button></td>
+      </tr>
+      <tr>
+        <td>fuck fuck fuck</td>
+        <td>ML</td>
+        <td>mary@example.com</td>
+        <td><button type="button" class="btn btn-primary">Interested</button></td>
+      </tr>
+      <tr>
+        <td>shit shit shit shits hsot</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+        <td><button type="button" class="btn btn-primary">Interested</button></td>
+      </tr>
+      <tr>
+        <td>shit shit shit shits hsot</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+        <td><button type="button" class="btn btn-primary">Interested</button></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
