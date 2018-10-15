b3ecbd053693c492cd15fd0be85b7bececff0582
pratikjain04 <pratikjain0411@gmail.com>
2018-10-15 12:50:51 +0530

    UniversityModel added
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

--- idky/lib/Projects/UniversityProject.dart
+++ idky/lib/Projects/UniversityProject.dart
@@ -8,45 +8,46 @@ class UniversityProject{
 
   UniversityProject(this.projectName, this.mentorName, this.domainName, this.description);
 
-  List<UniversityProject> universityProjects = [
 
-    UniversityProject(
+
+}
+
+
+List<UniversityProject> universityProjects = [
+
+  UniversityProject(
       'Smart Energy Rooms',
       'Pratik Jain',
       'Internet of Things',
       'Research Project for Energy Reduction'
-    ),
+  ),
 
-    UniversityProject(
-        'TechiDeate',
-        'Anshuman Kalla',
-        'Web Development',
-        'Website on PHP for Tech Fest'
-    ),
+  UniversityProject(
+      'TechiDeate',
+      'Anshuman Kalla',
+      'Web Development',
+      'Website on PHP for Tech Fest'
+  ),
 
-    UniversityProject(
-        'Marksheet Scanner',
-        'Ankit Sharma',
-        'Machine Learning',
-        'Uploads the marks of exams directly in one image'
-    ),
+  UniversityProject(
+      'Marksheet Scanner',
+      'Ankit Sharma',
+      'Machine Learning',
+      'Uploads the marks of exams directly in one image'
+  ),
 
-    UniversityProject(
-        'infoGIT',
-        'Aashis Kumar',
-        'App Development',
-        'App which gives all the information on GIT'
-    ),
+  UniversityProject(
+      'infoGIT',
+      'Aashis Kumar',
+      'App Development',
+      'App which gives all the information on GIT'
+  ),
 
-    UniversityProject(
-        'DroneAid',
-        'Pratik Jain',
-        'Internet of Things',
-        'Research Project for Disaster Management'
-    ),
+  UniversityProject(
+      'DroneAid',
+      'Pratik Jain',
+      'Internet of Things',
+      'Research Project for Disaster Management'
+  ),
 
-  ];
-
-
-
-}
+];
