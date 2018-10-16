ac87990f2a3715fc17883f24788d1dbf094efa04
ateek-ujjawal <ateekujjawal@gmail.com>
2018-10-16 14:11:48 +0530

    Add backend to University Project
    Signed-off-by: ateek-ujjawal <ateekujjawal@gmail.com>

--- idky/lib/Home/UniversityProjectModel.dart
+++ idky/lib/Home/UniversityProjectModel.dart
@@ -40,6 +40,12 @@ class ProjectCardWidget extends StatefulWidget {
 class _ProjectCardWidgetState extends State<ProjectCardWidget> {
 
   final _scaffoldKey = new GlobalKey<ScaffoldState>();
+  DocumentReference documentReference;
+
+  void _addOngoingProject(Map<String, String> data) async {
+    documentReference = Firestore.instance.document('projects/${data['projectName']}');
+    await documentReference.setData(data);
+  }
 
   void _showModalSheet() {
     showModalBottomSheet(
@@ -64,14 +70,21 @@ class _ProjectCardWidgetState extends State<ProjectCardWidget> {
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                           Padding(padding: EdgeInsets.only(top: 25.0)),
-                          Text(widget.universityProject.longdesc,),
+                          Text(widget.universityProject.longDesc,),
                           Padding(padding: EdgeInsets.only(top: 25.0)),
                           RaisedButton(
                             elevation: 5.0,
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(25.0)),
                             onPressed: () {
-                              //todo: ATEEK
+                              Map<String, String> data = <String, String>{
+                                "projectName" : widget.universityProject.projectName,
+                                "companyName" : widget.universityProject.companyName,
+                                "domainName" : widget.universityProject.domainName,
+                                "description" : widget.universityProject.description,
+                                "longDesc" : widget.universityProject.longDesc
+                              };
+                              _addOngoingProject(data);
                             },
                             child: Text('Join Project'),
                           )
@@ -118,7 +131,7 @@ class _ProjectCardWidgetState extends State<ProjectCardWidget> {
                       Padding(
                         padding: EdgeInsets.only(top: 5.0),
                         child: Text(
-                          'by ' + widget.universityProject.mentorName,
+                          'by ' + widget.universityProject.companyName,
                           style: TextStyle(
                               fontStyle: FontStyle.italic,
                               fontWeight: FontWeight.w300,
