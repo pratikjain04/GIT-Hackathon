be17ee3bc0ae8c114ac8e25cd57728e7d0f3a04d
ateek-ujjawal <ateekujjawal@gmail.com>
2018-10-16 10:04:49 +0530

    Add Skills Page Backend
    Signed-off-by: ateek-ujjawal <ateekujjawal@gmail.com>

--- idky/lib/Home/Skills.dart
+++ idky/lib/Home/Skills.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'SkillsModel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Skills extends StatefulWidget {
   @override
@@ -8,28 +11,86 @@ class Skills extends StatefulWidget {
 
 class _SkillsState extends State<Skills> {
 
-  _setValue(int index, bool value){
+  List<SkillsModel> savedSkills = skills;
+  DocumentReference documentReference;
+  bool changed = false;
+
+  void _setValue(int index, bool value){
     setState(() {
-      skills[index].isChecked = value;
+      savedSkills[index].isChecked = value;
+    });
+  }
+
+  Future<DocumentSnapshot> _getSavedSkills() async {
+    documentReference = Firestore.instance.document('user/skills');
+    DocumentSnapshot snapshot = await documentReference.get();
+    return snapshot;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getSavedSkills().then((docsnapshot){
+      docsnapshot.data['domains'].forEach((domain){
+          for(int i = 0; i < savedSkills.length; i++){
+            if(savedSkills[i].title == domain){
+              setState(() {
+                savedSkills[i].isChecked = true;
+                changed = true;
+              });
+            }
+        }
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: new ListView.builder(
-          shrinkWrap: true,
-          itemCount: 6,
-          itemBuilder: (BuildContext context, int index){
-            return CheckboxListTile(
-                controlAffinity: ListTileControlAffinity.leading,    //puts checkbox on the left
-                title: Text(skills[index].title),   //we only need the keys , i.e., the toppings and not the true and false values
-                value: skills[index].isChecked,       //Receives only true or false
-                onChanged: (bool value){_setValue(index,value);}
-            );
-          },
-        ),
+      appBar: AppBar(
+        title: Text('Select your skills'),
+        centerTitle: true,
+      ),
+      body: Stack(
+
+        children:
+        [
+          Column(
+            children: <Widget>[
+              (changed == true) ? Container(
+                child: new ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 6,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CheckboxListTile(
+                        controlAffinity: ListTileControlAffinity.leading,
+                        //puts checkbox on the left
+                        title: Text(savedSkills[index].title),
+                        //we only need the keys , i.e., the toppings and not the true and false values
+                        value: savedSkills[index].isChecked,
+                        //Receives only true or false
+                        onChanged: (bool value) {
+                          _setValue(index, value);
+                        }
+                    );
+                  },
+                ),
+              ) : CircularProgressIndicator(),
+              RaisedButton(
+                color: Colors.blue,
+                onPressed: () {
+
+                },
+                child: Text(
+                  'Submit Your Choices',
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+              )
+            ],
+          ),
+        ]
       ),
     );
   }
